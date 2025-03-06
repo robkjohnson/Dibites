da1015bd260b477823336207cb1161323336e00d
robkjohnson <robkjohnson@gmail.com>
2025-03-06 12:19:48 -0600

    Gave dashboard a title for browser tab

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,7 +4,7 @@ from layout import get_layout
 from callbacks import register_callbacks
 from utils import get_simulations_base_folder
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY], suppress_callback_exceptions=True)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY], suppress_callback_exceptions=True, title="Dibites")
 app.layout = get_layout()
 
 # Store the simulations base folder in a custom attribute if needed:
